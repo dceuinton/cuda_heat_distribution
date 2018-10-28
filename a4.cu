f55a0b57b3c8bad69c8ac0448c9a1543e135e4da
Dale Euinton <dale.euinton@gmail.com>
2018-10-28 16:01:17 +1300

    Got the parameters down, just need to write the function for the device.

--- a4.cu
+++ a4.cu
@@ -29,14 +29,49 @@ void debug(const char* format, ...) {
     #endif
 }
 
-__global__ void make_image(float* image, int size, float xSource, float ySource,
-                           float* xlens, float* ylens, float rsrc, float ldc) {
+void print(const char* format, ...) {
+    va_list args;
+    va_start(args, format);
+    // vfprintf(stdout, "", NULL);
+    vfprintf(stdout, format, args);
+    vfprintf(stdout, "\n", NULL);
+    va_end(args);
+    fflush(stdout);
+}
+
+// the lens image 
+// the size of lens image
+// the parameters of the image
+// lens scale
+// lens info, with eps & nlenses
+// source star info (x, y, radius^2)
+// lens darkening coefficient
+// Total: 15
+
+// Ones to copy over to memory:
+// image, xlens, ylens, eps
+
+__global__ void make_image(float* image, int size,                                  // lens image
+                           float XLeft, float XRight, float YBottom, float YTop,    // the size of lens image
+                           float lens_scale,                                        // lens scale
+                           float* xlens, float* ylens, float* eps, int num_lenses,  // lens info with eps and number of lenses
+                           float xSource, float ySource, float radSource2,          // source star information, x, y, radius^2
+                           float ldc) {                                             // lens darkening coefficient
 
 }
 
 __device__ void device_shoot(float& xSource, float& ySource, float xl, float yl,
                              float* xlens, float* ylens, float* eps, int nlenses) {
-
+    float dx, dy, dr;
+    xSource = xl;
+    ySource = yl;
+    for (int p = 0; p < nlenses; ++p) {
+        dx = xl - xlens[p];
+        dy = yl - ylens[p];
+        dr = dx * dx + dy * dy;
+        xSource -= eps[p] * dx / dr;
+        ySource -= eps[p] * dy / dr;
+    }
 }
 
 int main(int argc, char **argv) {
@@ -44,18 +79,18 @@ int main(int argc, char **argv) {
 
     // Setting values for the lens setting 
     float *xlens, *ylens, *eps;
-    const int nlenses = set_example_3(&xlens, &ylens, &eps);
-    cout << "Simulating " << nlenses << " lens system." << endl;
+    const int nlenses = set_example_1(&xlens, &ylens, &eps);
+    print("Simulating a %d lens system", nlenses);
     
     // Set the size of the lens image
     const float lens_scale = 0.005;
     const int npixx = static_cast<int>(floor((XL2 - XL1) / lens_scale)) + 1;
     const int npixy = static_cast<int>(floor((YL2 - YL1) / lens_scale)) + 1;
-    cout << "Building an " << npixx << "x" << npixy << " lens image." << endl;
+    print("Building a %d by %d image", npixx, npixy);
 
-    Array<float, 2> lens_image(npixy, npixx);    // The lens image
+    // Initialise image and get information
+    Array<float, 2> lens_image(npixy, npixx);   
     int size = lens_image.getSize();
-    float* device_image = lens_image.getBuffer();
 
     // Source star parameters. You can adjust these if you like - it is
     // interesting to look at the different lens images that result
@@ -63,23 +98,43 @@ int main(int argc, char **argv) {
     const float ldc  = 0.5;      // limb darkening coefficient
     const float xSource = 0.0;   // x and y centre on the map
     const float ySource = 0.0;
+    const float rsrc2 = rsrc * rsrc;
 
-
-
-
-
-    // Copy image to device
+    // Create some device addresses
+    float* device_image;
+    float* device_xlens;
+    float* device_ylens;
+    float* device_eps;
+    // initialise memory for device
     cudaMalloc(&device_image, size);
-    cudaMemcpy(device_image, device_image, size, cudaMemcpyHostToDevice);
+    cudaMalloc(&device_xlens, nlenses);
+    cudaMalloc(&device_ylens, nlenses);
+    cudaMalloc(&device_eps, nlenses);
 
-    
+    // Copy memory over onto device
+    cudaMemcpy(device_image, lens_image.getBuffer(), size, cudaMemcpyHostToDevice);
+    cudaMemcpy(device_xlens, xlens, nlenses, cudaMemcpyHostToDevice);
+    cudaMemcpy(device_ylens, ylens, nlenses, cudaMemcpyHostToDevice);
+    cudaMemcpy(device_eps, eps, nlenses, cudaMemcpyHostToDevice);
 
-    
+    int threads_per_block = 256;
+    int blocks_per_grid = (size + threads_per_block - 1) / threads_per_block;
 
+    debug("Dimensions are tpb: %d, bpg: %d", threads_per_block, blocks_per_grid);
+    print("Launching a grid of %d %d threads", blocks_per_grid, threads_per_block*blocks_per_grid);
 
+    make_image<<<blocks_per_grid, threads_per_block>>>(device_image, size,
+                                                       XL1, XL2, YL1, YL2,
+                                                       lens_scale,
+                                                       device_xlens, device_ylens, device_eps, nlenses,
+                                                       xSource, ySource, rsrc2,
+                                                       ldc);
 
-
-
+    // Free the memory
+    cudaFree(device_image);
+    cudaFree(device_xlens);
+    cudaFree(device_ylens);
+    cudaFree(device_eps);
 
     debug("Finished");
 }
